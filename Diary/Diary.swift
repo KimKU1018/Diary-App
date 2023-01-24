6d46476bfd4f4f950847b5884956da987dcea4d6
KimKU1018 <86593582+KimKU1018@users.noreply.github.com>
2023-01-24 21:47:31 +0900

    ✨ 일기 작성 기능 구현

---
+++ Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by KU Kim on 2023/01/24.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
