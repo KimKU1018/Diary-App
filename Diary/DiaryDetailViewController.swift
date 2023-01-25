3e05acca8a3bc0d9585622ba080fbee1d3354237
KimKU1018 <86593582+KimKU1018@users.noreply.github.com>
2023-01-25 14:55:03 +0900

    ✨ 일기 수정 기능 구현

--- Diary/DiaryDetailViewController.swift
+++ Diary/DiaryDetailViewController.swift
@@ -40,8 +40,27 @@ class DiaryDetailViewController: UIViewController {
         return formatter.string(from: date)
     }
     
+    @objc func editDiaryNotification(_ notification:Notification){
+        guard let diary = notification.object as? Diary else { return }
+        guard let row = notification.userInfo?["indexPath.row"] as? Int else { return }
+        self.diary = diary
+        self.configureView()
+        
+        
+    }
+    
 
     @IBAction func tapEditButton(_ sender: UIButton) {
+        guard let viewControlller = self.storyboard?.instantiateViewController(identifier: "WriteDiaryViewController") as? WriteDiaryViewController else {  return }
+        guard let indexPath = self.indexPath else { return }
+        guard let diary = self.diary else { return }
+        viewControlller.diaryEditorMode = .edit(indexPath, diary)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(editDiaryNotification(_:)),
+            name: NSNotification.Name("editDiary"),
+            object: nil)
+        self.navigationController?.pushViewController(viewControlller, animated: true)
     }
     
 
@@ -49,5 +68,9 @@ class DiaryDetailViewController: UIViewController {
         guard let indexPath = self.indexPath else { return }
         self.delegate?.didSelectDelete(indexPath: indexPath)
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
